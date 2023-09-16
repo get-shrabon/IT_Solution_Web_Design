45b81149498d6e7564ee37f446f8f5668480b20b
Shrabon Talukder <shrabontalukder425@gmail.com>
2023-09-16 20:07:37 +0600

    Add Hero Section

--- assets/JS/custom.js
+++ assets/JS/custom.js
@@ -1,24 +1,37 @@
 // Nav Bar Sticky JS
-
-
-
-
-
-
-
-
-
-
-window.onscroll = function() {myFunction()};
+window.onscroll = function () {
+  myFunction();
+};
 
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
   }
 }
 // Nav Bar Sticky JS
+
+// Swiper Slider
+var swiper = new Swiper(".heroSlider", {
+    spaceBetween: 30,
+    effect: "fade",
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+      clickable:true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type:"fraction",
+      clickable: true,
+    },
+  });
+// Swiper Slider
