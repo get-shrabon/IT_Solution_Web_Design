8a1d3da74eec0501e6f2bdf42b8f2aa78cec40cc
Shrabon Talukder <shrabontalukder425@gmail.com>
2023-09-17 19:17:45 +0600

    Complete Expertise Section

--- assets/JS/custom.js
+++ assets/JS/custom.js
@@ -17,26 +17,52 @@ function myFunction() {
 
 // Swiper Slider
 var swiper = new Swiper(".heroSlider", {
-    spaceBetween: 30,
-    effect: "fade",
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-      },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-      clickable:true,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      type:"fraction",
-      clickable: true,
-    },
-  });
+  spaceBetween: 30,
+  effect: "fade",
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    clickable: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+    clickable: true,
+  },
+});
 // Swiper Slider
 
 // Partner Slider JS
 var copy = document.querySelector(".partner_items").cloneNode(true);
 document.querySelector(".myPartners").appendChild(copy);
 // Partner Slider JS
+
+// Expertise Slider JS
+var swiper = new Swiper(".myExpertise", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    400: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+});
+// Expertise Slider JS
